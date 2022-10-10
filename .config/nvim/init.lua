ca4fd3d89c983e132c1562cb73ca21b8cc9edbc6
anzhi0708 <anzhi0708@gmail.com>
2022-10-11 02:28:44 +0900

    ...

---
+++ .config/nvim/init.lua
@@ -0,0 +1,10 @@
+vim.g.mapleader = ","
+vim.o.number = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.relativenumber = true
+vim.o.shortmess = "filnxtToOFI"
+vim.o.cursorline = true
+vim.o.cursorcolumn = true
+vim.o.scrolloff = 9
