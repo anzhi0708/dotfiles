32bbb0f3de8657f789ca55a07acc62c3f101ceef
anzhi0708 <anzhi0708@gmail.com>
2022-11-06 05:57:07 +0900

    updated init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,5 @@
+require 'plugins'
+
 vim.g.mapleader = ","
 vim.o.number = true
 vim.o.tabstop = 2
@@ -8,3 +10,43 @@ vim.o.shortmess = "filnxtToOFI"
 vim.o.cursorline = true
 vim.o.cursorcolumn = true
 vim.o.scrolloff = 9
+
+local configs = require "lspconfig.configs"
+local lspconfig = require 'lspconfig'
+
+if not configs.lua_language_server then
+	configs.lua_language_server = {
+    default_config = {
+      cmd = { 'lua-language-server' },
+      filetypes = { 'lua' },
+			init_options = {},
+			root_dir = function(fname)
+				return vim.fn.getcwd()
+			end,
+			autostart = true
+    },
+  }
+end
+
+-- Lua language server
+lspconfig.lua_language_server.setup{}
+
+-- Rust language server
+lspconfig.rust_analyzer.setup{}
+
+-- Python language server
+if not configs.pylsp then
+	configs.pylsp = {
+    default_config = {
+      cmd = { 'pylsp' },
+      filetypes = { 'python' },
+			init_options = {},
+			root_dir = function(fname)
+				return vim.fn.getcwd()
+			end,
+			autostart = true
+    },
+  }
+end
+
+lspconfig.pylsp.setup{}
